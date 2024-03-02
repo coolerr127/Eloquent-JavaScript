4dcfa49ea905c3ce59f78d4102e56c8c2dae1ab8
Anton Shimanovich <ashimanovich127@gmail.com>
2024-03-02 12:35:16 +0400

    refactor: chapter 2

--- chapter_2_prog_structure/chapter_2.js
+++ chapter_2_prog_structure/chapter_2.js
@@ -1,75 +1,34 @@
-// looping a triangle
-let str = ''
-while (str.length < 7) {
-	str += '#'
-	console.log(str)
+// #1 looping a triangle
+{
+  console.info("---------- #1 looping a triangle ----------");
+
+  for (let i = 1; i < 8; i++) console.log("#".repeat(i));
 }
-//---solution---
-//for (let line = '#'; line.length < 8; line += '#') console.log(line)
 
-//FizzBuzz
-for (let i = 1; i < 100; i++) {
-	if (i % 3 === 0 && i % 5 === 0) {
-		console.log('FizzBuzz')
-		continue
-	} else if (i % 3 === 0) {
-		console.log('Fizz')
-		continue
-	} else if (i % 5 === 0) {
-		console.log('Buzz')
-		continue
-	} else {
-		console.log(i)
-	}
+// #2 FizzBuzz
+{
+  console.info("---------- #2 FizzBuzz ----------");
+
+  let output = Array.from({ length: 100 }, (_, i) =>
+    (i + 1) % 3 === 0 ? "Fizz" : (i + 1) % 5 === 0 ? "Buzz" : i + 1,
+  ).join(" ");
+
+  console.log(output);
 }
-//---solution---
-// for (let n = 1; n <= 100; n++) {
-// 	let output = ''
-// 	if (n % 3 == 0) output += 'Fizz'
-// 	if (n % 5 == 0) output += 'Buzz'
-// 	console.log(output || n)
-// }
 
-// chessboard
-const size = 10
-let hash = true
-let test = false
-let str = ''
-for (let i = 0; i < size; i++) {
-	for (let j = 0; j < size; j++) {
-		if (j === 0) {
-			str += '\n'
-			if (test) {
-				str += ' '
-				test = !test
-			} else {
-				test = !test
-			}
-		}
-		if (hash) {
-			str += '#'
-			hash = !hash
-		} else if (!hash) {
-			str += ' '
-			hash = !hash
-		}
-	}
+// #3 chessboard
+{
+  console.info("---------- #3 chessboard ----------");
+
+  const size = 10;
+  let board = "";
+
+  for (let y = 0; y < size; y++) {
+    for (let x = 0; x < size; x++) {
+      board += (x + y) % 2 === 0 ? " " : "#";
+    }
+    board += "\n";
+  }
+
+  console.log(board);
 }
-console.log(str)
-//---solution---
-// let size = 8
-
-// let board = ''
-
-// for (let y = 0; y < size; y++) {
-// 	for (let x = 0; x < size; x++) {
-// 		if ((x + y) % 2 == 0) {
-// 			board += ' '
-// 		} else {
-// 			board += '#'
-// 		}
-// 	}
-// 	board += '\n'
-// }
-
-// console.log(board)
